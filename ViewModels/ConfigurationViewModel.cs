4dd20b7566fd5798e1cc97b7c8f4900e9949697f
Jens Eresund <jens.eresund@gmail.com>
2024-10-21 12:04:34 +0200

    Add project files.

---
+++ ViewModels/ConfigurationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb3_CsProg_ITHS.NET.ViewModels;
+internal class ConfigurationViewModel
+{
+}
